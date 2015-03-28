977bd027c18715068f11c76ba30aee1f1ed63fec
odekeyser <dksrolivier@gmail.com>
2015-03-28 04:36:35 +0100

    addPlaceActivity : Begin

---
+++ app/src/main/java/com/example/thrag/annuaire/AddPlaceActivity.java
@@ -0,0 +1,19 @@
+package com.example.thrag.annuaire;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+/**
+ * Created by Thrag on 28/03/15.
+ */
+public class AddPlaceActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addplace);
+
+
+    }
+}
